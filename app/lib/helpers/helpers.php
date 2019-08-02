74568149ff5539c5da17800ab9bec28fc0c9853f
tanzorr <alexx1984@ukr.net>
2019-08-02 15:27:38 +0300

    finish model

--- app/lib/helpers/helpers.php
+++ app/lib/helpers/helpers.php
@@ -12,3 +12,7 @@ function did($data){
     echo '</pre>';
     die();
 }
+
+function sanitize($dirty){
+    return htmlentities($dirty, ENT_QUOTES, 'UTF-8');
+}
--- core/Model.php
+++ core/Model.php
@@ -31,4 +31,88 @@ class Model
         return $this->_db->get_columns($this->_table);
     }
 
+    public  function find($params = []){
+            $results = [];
+            $resultsQuery =  $this->_db->find($this->_table, $params);
+            foreach ($resultsQuery as $result){
+                $obj = new $this->_modulName($this->_table);
+                $obj->populateObjData($result);
+                $result[] = $obj;
+            }
+            return $results;
+    }
+
+    public  function  findFirst($params= []){
+        $resultQuery = $this->_db->findFirst($this->_table, $params);
+        $result =  new $this->_modulName($this->_table);
+        $result->populateObjData($resultQuery);
+        return $result;
+    }
+
+    public  function findById($id){
+        return $this->findFirst(['conditions'=>"id=?", 'bind'=>[$id]]);
+    }
+
+    public function save(){
+        $fildes =[];
+        foreach ($this->_columnNames as $column){
+            $fildes[$column] =  $this->$column;
+        }
+        //detemine whether to update or insert
+        if(property_exists($this,'id') && $this->id !== ''){
+            return$this->update($this->id, $fildes);
+        }else{
+            return $this->insert($fildes);
+        }
+    }
+
+    public  function insert($fieldes){
+        if(empty($fields)) return false;
+        return $this->_db->insert($this->_table, $fields);
+    }
+
+    public function update($id, $fields){
+        if(empty($fields) || $id=='') return false;
+        return $this->_db->update($this->_table, $id, $fields);
+    }
+
+    public  function delete($id = ''){
+        if($id == '' &&  $this->id='') return false;
+        $id -($id=='')? $this->id : $id;
+        if($this->_softDelete){
+            return $this->update($id,['deleted'=>1]);
+        }
+        return $this->delete($this->_table. $id);
+    }
+
+    public function query($sql,$bind){
+        return $this->_db->query($sql,$bind=[]);
+    }
+
+    public function data(){
+        $data =  new stdClass();
+        foreach ($this->_columnNames as $colum){
+            $data->column = $this->column;
+        }
+        return $data;
+    }
+
+    public function assign($params){
+        if(!empty($params)){
+            foreach ($params as $key=>$val) {
+                if(in_array($key, $this->_columnNames)){
+                    $this->$key =  sanitize($val);
+                }
+            }
+            return true;
+        }
+    }
+    
+    protected function  populateObjData($result){
+        foreach ($result as $key=>$val){
+            $this->$key = $val;
+        }
+    }
+    
+
 }
